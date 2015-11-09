726acc0420859b265a0118c1d2b6612fb14d920f
Jad Wahab <jad_wahab@hotmail.com>
2015-11-09 21:24:40 +0200

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Jad Wahab on 11/9/15.
+//  Copyright © 2015 Jad Wahab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
